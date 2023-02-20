9e54bd04e1b7a131238d03678f367100dfbff796
Nalbert de Jesus <nalbert-edm@outlook.com>
2023-02-20 15:47:03 -0300

    Criado serviço de autenticação iron session

--- pages/api/user/signup.js
+++ pages/api/user/signup.js
@@ -1,8 +1,13 @@
 import Joi from "joi";
+import { withIronSessionApiRoute } from 'iron-session/next'
+
 import createHandler from "../../../lib/middlewares/nextConnect";
 
 import validate from "../../../lib/middlewares/validation";
 import { signupUser } from "../../../modules/user/user.service";
+
+import { ironConfig } from "../../../lib/middlewares/ironSession";
+
 
 const postSchema = Joi.object({
   name: Joi.string().required().max(50),
@@ -16,7 +21,13 @@ const signup = createHandler();
 signup.post(validate({ body: postSchema }), async (req, res) => {
   try{
     const user = await signupUser(req.body);
-    res.status(201).json(user);
+    req.session.user = {
+      id: user._id,
+      user: user.user
+    }
+    await req.session.save()
+
+    res.status(201).json({ok: true});
   } catch (err){
     console.log(err)
     throw err
@@ -24,4 +35,4 @@ signup.post(validate({ body: postSchema }), async (req, res) => {
 
 });
 
-export default signup;
+export default withIronSessionApiRoute(signup, ironConfig);
